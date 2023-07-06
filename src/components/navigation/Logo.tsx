d2652c5ba36ef26a86a3d043570aac569cd33207
Emanuel Bodin <embodin@kth.se>
2023-07-06 14:19:41 +0200

    fixed logo

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -1,20 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Logo() {
   return (
-    <div className="flex gap-1 cursor-pointer">
-      <a
-        className="text-gray-100 text-base xl:text-xl no-underline hover:no-underline font-bold text-center"
-        href="/">
-        Run
-      </a>
+    <Link
+      className="flex items-center gap-1 cursor-pointer text-gray-100 text-base no-underline hover:no-underline font-bold text-center"
+      href="/">
+      Run
       <Image
-        width={40}
-        height={40}
-        style={{ width: '40px', height: 'auto', objectFit: 'contain' }}
+        width={30}
+        height={30}
+        style={{ width: '30px', height: 'auto', objectFit: 'contain' }}
         src="/running.svg"
         alt="running-logo"
       />
-    </div>
+    </Link>
   )
 }
